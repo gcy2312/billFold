f473d470aff831ed9cf2225e90900c2bf7f8d419
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-10-16 17:38:34 -0400

    getExpenses() - return [], edit expense & create expense dialog (populate w data from getExpenses()

--- src/app/expenses-page/expenses-page.component.ts
+++ src/app/expenses-page/expenses-page.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { ExpenseDetailsComponent } from '../expense-details/expense-details.component';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ExpenseEditComponent } from '../expense-edit/expense-edit.component';
+import { ExpenseCreateComponent } from '../expense-create/expense-create.component';
+
 
 @Component({
   selector: 'app-expenses-page',
@@ -6,10 +14,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expenses-page.component.scss']
 })
 export class ExpensesPageComponent implements OnInit {
+  expenses: any[] = [];
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
+    this.getExpenses(this.userId, this.token);
+  }
+
+  getExpenses(userId: string, token: string): void {
+    this.fetchApiData.getExpenses(userId, token).subscribe((resp: any) => {
+      this.expenses = resp;
+      console.log(this.expenses);
+      return this.expenses;
+    });
+  }
+
+  // getExpenseById(expenseId:string, token:string): void {
+  //   this.fetchApiData.getExpenseById(expenseId, token).subscribe((resp:any)=>{
+
+  //   })
+  // }
+
+  // getExpenseDetails(_id: string, Category: string, Description: string, Date: string, Amount: string, Currency: string): void {
+  //   this.dialog.open(ExpenseDetailsComponent, {
+  //     data: { Category: Category, Description: Description, Date: Date, Amount: Amount, Currency: Currency },
+  //     width: '500px'
+  //   });
+  // }
+
+  openEditExpenseDialog(_id: string, Category: string, Description: string, Date: string, Amount: string, Currency: string): void {
+    this.dialog.open(ExpenseEditComponent, {
+      data: { Category: Category, Description: Description, Date: Date, Amount: Amount, Currency: Currency },
+      width: '500px'
+    });
+  }
+
+  openCreateExpenseDialog(): void {
+    this.dialog.open(ExpenseCreateComponent, {
+      width: '280px'
+    });
   }
 
 }
