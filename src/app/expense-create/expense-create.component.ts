787861bb64628eccae7f814c5fe74c9ff1db2b7a
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-10-16 15:10:36 -0400

    create expense-create comp

---
+++ src/app/expense-create/expense-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-create',
+  templateUrl: './expense-create.component.html',
+  styleUrls: ['./expense-create.component.scss']
+})
+export class ExpenseCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
