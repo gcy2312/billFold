e763bdb424c8af1c2854cfe3499eafdc24013270
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-10-16 17:38:55 -0400

    delete user - nav to /welcome

--- src/app/user-profile-delete/user-profile-delete.component.ts
+++ src/app/user-profile-delete/user-profile-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-profile-delete',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileDeleteComponent implements OnInit {
 
-  constructor() { }
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  deleteUserAccount(token: string, userId: string): void {
+    this.fetchApiData.deleteUser(userId, token).subscribe((resp: any) => {
+      this.snackBar.open(
+        'Your account has been deleted!', 'OK', {
+        duration: 2000,
+      }
+      );
+      localStorage.clear();
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000,
+      });
+      this.router.navigate(['/welcome']).then(() => {
+        window.location.reload();
+      });
+    }
+    );
+  }
+
+  cancel(): void {
+    this.router.navigate(['/profile']).then(() => {
+      window.location.reload();
+    });
+  }
+
 
 }
