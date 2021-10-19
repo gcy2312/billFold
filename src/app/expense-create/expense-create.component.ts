b9410e34f587c19713bb543661b875760dedeb3e
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-10-19 19:57:32 -0400

    create partial expense type and createExpense()

--- src/app/expense-create/expense-create.component.ts
+++ src/app/expense-create/expense-create.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
+
+
+const PartialExpense = {
+  Category: '',
+  Description: '',
+  Date: '',
+  Amount: { $numberDecimal: '' },
+  Currency: '',
+};
+
 
 @Component({
   selector: 'app-expense-create',
@@ -6,10 +20,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expense-create.component.scss']
 })
 export class ExpenseCreateComponent implements OnInit {
+  @Input() expenseInfo = PartialExpense;
 
-  constructor() { }
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<ExpenseCreateComponent>,
+    public snackBar: MatSnackBar) { }
+
 
   ngOnInit(): void {
+  }
+
+  createExpense(userId: string, token: string): void {
+    this.fetchApiData.createExpense(this.expenseInfo, userId, token).subscribe((result) => {
+      // Logic for a successful user registration goes here! (To be implemented)
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open('Expense document successfully added', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
   }
 
 }
