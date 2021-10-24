c36bfc60efb367cd0d77500a2e800e3e8f70d89b
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-10-24 19:36:32 -0400

    PartialBill type, + createBill (problem - not adding to bills [] )

--- src/app/bill-create/bill-create.component.ts
+++ src/app/bill-create/bill-create.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+
+
+const PartialBill = {
+  Description: '',
+  Date: '',
+  Amount: { $numberDecimal: '' },
+  Currency: '',
+  Paid: false
+};
 
 @Component({
   selector: 'app-bill-create',
   templateUrl: './bill-create.component.html',
   styleUrls: ['./bill-create.component.scss']
 })
+
 export class BillCreateComponent implements OnInit {
 
-  constructor() { }
+  @Input() billInfo = PartialBill;
+
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
+
+  isChkChecked = false;
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<BillCreateComponent>,
+    public snackBar: MatSnackBar,
+
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
 
   ngOnInit(): void {
+    console.log(this.userId);
+  }
+
+  createBill(userId: string, token: string): void {
+    this.fetchApiData.createExpense(this.billInfo, userId, token).subscribe((result) => {
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open('Bill document successfully added', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  onChkChange(ob: MatCheckboxChange) {
+    if (ob.checked) {
+      this.billInfo.Paid = true;
+    } else {
+      this.billInfo.Paid = false;
+    }
   }
 
 }
