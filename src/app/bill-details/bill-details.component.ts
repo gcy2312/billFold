7962338c57aac1cf5ffbd933f02ee7ade1860b8f
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-10-25 13:06:58 -0400

    trnasferBillData (mat data from bill-page), data for editBill (Paid)

--- src/app/bill-details/bill-details.component.ts
+++ src/app/bill-details/bill-details.component.ts
@@ -1,22 +1,80 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+
+const transferBillData = {
+  title: '',
+  Amount: { $numberDecimal: '' },
+  date: '',
+  Paid: false,
+  Currency: '',
+  UserId: '',
+  _id: '',
+}
+const PartialBill = {
+  Paid: false,
+}
 
 @Component({
   selector: 'app-bill-details',
   templateUrl: './bill-details.component.html',
   styleUrls: ['./bill-details.component.scss']
 })
+
 export class BillDetailsComponent implements OnInit {
+  @Input() billDetails = transferBillData; billInfo = PartialBill;
+
+
+
+  isChkChecked = false;
+
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
 
   constructor(
     public dialogRef: MatDialogRef<BillDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+
   ) { }
 
   ngOnInit(): void {
+    this.billDetails = this.data;
+
+    this.billInfo.Paid = this.data.Paid;
+    this.isChkChecked = this.billInfo.Paid;
+
+    console.log('title: ' + this.billDetails.title);
+    console.log('billId: ' + this.billDetails._id);
   }
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  onChkChange(ob: MatCheckboxChange) {
+    if (ob.checked) {
+      this.billInfo.Paid = true;
+    } else {
+      this.billInfo.Paid = false;
+    }
+  }
+
+  billId = this.data._id;
+
+  editBill(userId: string, token: string): void {
+    this.fetchApiData.editBill(this.billInfo, this.billId, token, userId).subscribe((result) => {
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open('Bill document successfully modified', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
   }
 
 }
