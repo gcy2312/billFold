5f834e1a898a9962cf4276f9b466075c5ee3a1ac
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-11-09 16:33:25 -0500

    editExpense (not working - convert to Decimal128)

--- src/app/expense-edit/expense-edit.component.ts
+++ src/app/expense-edit/expense-edit.component.ts
@@ -1,6 +1,19 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
 import { Expense } from '../types';
+
+const PartialExpense = {
+  _id: '',
+  Category: '',
+  Description: '',
+  Date: '',
+  Amount: { $numberDecimal: '' },
+  Currency: '',
+};
 
 @Component({
   selector: 'app-expense-edit',
@@ -8,15 +21,43 @@ import { Expense } from '../types';
   styleUrls: ['./expense-edit.component.scss']
 })
 export class ExpenseEditComponent implements OnInit {
+  @Input() expenseInfo = PartialExpense;
+
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
+  // expenseId = this.expenseInfo._id;
 
   constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<ExpenseEditComponent>,
+    public snackBar: MatSnackBar,
     @Inject(MAT_DIALOG_DATA)
     public data: Expense
   ) { }
 
-
-
   ngOnInit(): void {
+    console.log('expense id: ' + this.data._id);
+    console.log('userId: ' + this.userId);
+    console.log('token: ' + this.token);
   }
+
+  expenseId = this.data._id;
+
+
+
+  editExpense(userId: string, token: string, expenseId: string): void {
+    this.fetchApiData.editExpense(this.expenseInfo, expenseId, token, userId).subscribe((result) => {
+      this.dialogRef.close();
+      this.snackBar.open('Expense document successfully updated', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+
 
 }
