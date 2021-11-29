7898f5b0dae8bb2b02c34774441328505ab27e95
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-11-29 09:29:01 -0500

    updpate user in profile comp?

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
 
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { UserProfileUpdateComponent } from '../user-profile-update/user-profile-update.component';
@@ -23,6 +24,7 @@ export class UserProfileComponent implements OnInit {
     public dialog: MatDialog,
     public snackBar: MatSnackBar,
     public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserProfileComponent>
   ) { }
 
   ngOnInit(): void {
@@ -48,5 +50,25 @@ export class UserProfileComponent implements OnInit {
       width: '500px'
     });
   }
+
+  // updateUser(userData: Partial<User>, token: string, userId: string): void {
+  //   this.fetchApiData.editUser(userData, userId, token).subscribe((resp) => {
+  //     this.dialogRef.close(); //this will close modal on success
+  //     console.log(resp);
+  //     localStorage.setItem('userId', resp._id);
+  //     this.snackBar.open('Your profile has been successfully updated!', 'OK', {
+  //       duration: 2000,
+  //     });
+  //   }, (resp) => {
+  //     console.log(resp);
+  //     this.snackBar.open(resp, 'OK', {
+  //       duration: 2000,
+  //     });
+  //   });
+  //   setTimeout(function () {
+  //     window.location.reload();
+  //   }, 1000);
+  //   this.getUser(this.userId, this.token);
+  // }
 
 }
