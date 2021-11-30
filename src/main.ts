800f0ffc57b2bd252418682bf4ba5c0c488f6a29
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-11-30 13:12:09 -0500

    bug fix rename

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
+// import { AppModule } from './app/app.module';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
