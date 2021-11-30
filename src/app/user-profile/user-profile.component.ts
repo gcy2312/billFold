7d2f52b02fe101e21a7fd54108c5c9b0c4eae449
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-11-29 20:13:03 -0500

    open dialog for update

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -6,6 +6,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { UserProfileUpdateComponent } from '../user-profile-update/user-profile-update.component';
 import { UserProfileDeleteComponent } from '../user-profile-delete/user-profile-delete.component';
+import { UserUpdateUsernameComponent } from '../user-update-username/user-update-username.component';
 import { User } from '../types';
 
 @Component({
@@ -29,6 +30,7 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUser(this.userId, this.token);
+
   }
 
   getUser(userId: string, token: string): void {
@@ -36,12 +38,42 @@ export class UserProfileComponent implements OnInit {
       this.userData = resp;
       this.user = resp;
       console.log('user: ' + this.user._id);
+      console.log('name' + this.user.FirstName + this.user.LastName);
     });
   }
 
-  openUpdateDialog(): void {
-    this.dialog.open(UserProfileUpdateComponent, {
-      width: '500px'
+  openUpdateDialog(user: Partial<User>) {
+
+    const dialogRef = this.dialog.open(UserProfileUpdateComponent, {
+      data: {
+        FirstName: user.FirstName,
+        LastName: user.LastName,
+        Username: user.Username,
+        Email: user.Email,
+        Currency: user.CurrencyPref,
+        Password: user.Password
+      }
+      // width: '500px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+    this.getUser(this.userId, this.token);
+  }
+  openUpdateUsernameDialog(user: any) {
+    const dialogRef = this.dialog.open(UserUpdateUsernameComponent, {
+      data: {
+        FirstName: user.FirstName,
+        LastName: user.LastName,
+        Username: user.Username,
+        Email: user.Email,
+        Currency: user.CurrencyPref,
+        Password: user.Password
+      }
+      // width: '500px'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
     });
   }
 
