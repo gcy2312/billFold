e74b3e3c749c841575a1dea30ae85bd5582df190
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-12-01 13:27:44 -0500

    create update password component

---
+++ src/app/user-update-password/user-update-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-update-password',
+  templateUrl: './user-update-password.component.html',
+  styleUrls: ['./user-update-password.component.scss']
+})
+export class UserUpdatePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
