95cdd30961c4378fad0d1ddf65b8e601ee55c48b
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-12-01 13:14:12 -0500

    update name component

---
+++ src/app/user-update-name/user-update-name.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, Input, Inject } from '@angular/core';
+
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+const PartialUser = {
+  FirstName: '',
+  // LastName: '',
+}
+
+@Component({
+  selector: 'app-user-update-name',
+  templateUrl: './user-update-name.component.html',
+  styleUrls: ['./user-update-name.component.scss']
+})
+export class UserUpdateNameComponent implements OnInit {
+  @Input()
+  userData = PartialUser;
+
+  user: any = {};
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<UserUpdateNameComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { }
+
+  ngOnInit(): void {
+    console.log(this.data.FirstName);
+  }
+
+  updateUser(token: string, userId: string): void {
+    this.fetchApiData.editUser(this.userData, token, userId).subscribe((resp) => {
+      this.dialogRef.close(); //this will close modal on success
+      console.log(resp);
+      localStorage.setItem('user', resp.Username);
+      localStorage.setItem('userId', resp._id);
+      this.snackBar.open('Your name has been successfully updated!', 'OK', {
+        duration: 2000,
+      });
+    }, (resp) => {
+      console.log(resp);
+      this.snackBar.open(resp, 'OK', {
+        duration: 2000,
+      });
+    });
+    setTimeout(function () {
+      window.location.reload();
+    }, 1000);
+  }
+
+}
