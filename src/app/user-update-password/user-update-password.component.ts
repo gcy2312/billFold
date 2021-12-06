a6ea2c473d71dada8bebb00a4e5e928d88e17c70
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-12-06 10:21:37 -0500

    password update + show/hide

--- src/app/user-update-password/user-update-password.component.ts
+++ src/app/user-update-password/user-update-password.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+const PartialUser = {
+  Password: ''
+}
 
 @Component({
   selector: 'app-user-update-password',
@@ -6,10 +16,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-update-password.component.scss']
 })
 export class UserUpdatePasswordComponent implements OnInit {
+  @Input()
+  userData = PartialUser;
 
-  constructor() { }
+  user: any = {};
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<UserUpdatePasswordComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  newPassword: FormGroup = new FormGroup({
+    password: new FormControl('', [Validators.required, Validators.min(3)])
+  });
+  hide = true;
+  get passwordInput() { return this.newPassword.get('password'); }
+
+  updateUser(token: string, userId: string): void {
+    this.fetchApiData.editUser(this.userData, token, userId).subscribe((resp) => {
+      this.dialogRef.close(); //this will close modal on success
+      console.log(resp);
+      localStorage.setItem('user', resp.Username);
+      localStorage.setItem('userId', resp._id);
+      this.snackBar.open('Your password has been successfully updated!', 'OK', {
+        duration: 2000,
+      });
+    }, (resp) => {
+      console.log(resp);
+      this.snackBar.open(resp, 'OK', {
+        duration: 2000,
+      });
+    });
+    setTimeout(function () {
+      window.location.reload();
+    }, 1000);
   }
 
 }
