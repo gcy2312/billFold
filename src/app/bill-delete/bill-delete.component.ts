406d67ee96a8f6e5b4a04193260de0e90770f903
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-12-26 14:17:46 -0500

    deleteBill

--- src/app/bill-delete/bill-delete.component.ts
+++ src/app/bill-delete/bill-delete.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, Inject, Input } from '@angular/core';
+
+
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 import { Bill } from '../types';
 
@@ -8,6 +15,9 @@ const PartialBill = {
   Date: '',
   Amount: { $numberDecimal: '' },
   Currency: '',
+  UserId: '',
+  Index: true,
+  Paid: false,
 };
 
 @Component({
@@ -20,14 +30,48 @@ export class BillDeleteComponent implements OnInit {
 
   userId = localStorage.getItem('userId') || '';
   token = localStorage.getItem('token') || '';
+  billId: string = '';
 
   constructor(
-    @Inject(MAT_DIALOG_DATA)
-    public data: Bill
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<BillDeleteComponent>,
+    public router: Router,
+    @Inject(MAT_DIALOG_DATA) public data: any
 
   ) { }
 
   ngOnInit(): void {
+    console.log(this.data.bill);
+    this.billInfo = this.data.bill;
+    this.billId = this.data.bill._id;
+  }
+
+  deleteBill(billId: string, token: string): void {
+
+    this.fetchApiData.deleteBill(billId, token).subscribe((resp: any) => {
+      this.dialogRef.close();
+      this.snackBar.open(
+        'Bill document has been deleted!', 'OK', {
+        duration: 2000,
+      }
+      );
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000,
+      });
+      this.router.navigate(['/calendar']).then(() => {
+        window.location.reload();
+      });
+    }
+    );
+  }
+
+  cancel(): void {
+    this.router.navigate(['/calendar']).then(() => {
+      window.location.reload();
+    });
   }
 
 }
