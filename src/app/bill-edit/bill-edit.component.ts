a9c095861fb720e79da5d91144d203a0488f7431
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-12-26 14:19:38 -0500

    bill edit (billInfo = data MAT_DIALOG_DATA) edit Amount(already mapped to string***)

---
+++ src/app/bill-edit/bill-edit.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, Inject, Input } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+const PartialBill = {
+  _id: '',
+  Description: '',
+  Amount: '',
+  Date: '',
+  Currency: '',
+  Paid: false,
+  Index: true
+}
+
+@Component({
+  selector: 'app-bill-edit',
+  templateUrl: './bill-edit.component.html',
+  styleUrls: ['./bill-edit.component.scss']
+})
+
+
+export class BillEditComponent implements OnInit {
+  @Input() billInfo = PartialBill;
+
+  userId = localStorage.getItem('userId') || '';
+  token = localStorage.getItem('token') || '';
+  billId: string = '';
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<BillEditComponent>,
+    public snackBar: MatSnackBar,
+  ) { }
+
+  ngOnInit(): void {
+    console.log(this.data.bill);
+    this.billInfo = this.data.bill;
+    this.billId = this.data.bill._id;
+    console.log(this.billInfo);
+  }
+
+  editBill(userId: string, token: string, billId: string): void {
+    this.fetchApiData.editBill(this.billInfo, billId, token, userId).subscribe((result) => {
+      this.dialogRef.close();
+      this.snackBar.open('Bill document was successfully modified', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
