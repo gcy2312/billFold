99077df2bb54a7a26315ef448c1bea2492a2c17b
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-01-19 17:39:26 -0500

    nav bar and styling responsive done

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+  @Input()
+  // disablePagination: boolean;
+
+  navLinks = [
+    { path: '/expenses', label: 'Expenses', icon: 'insert_chart_outlined_two_tone' },
+    { path: '/calendar', label: 'Calendar', icon: 'calendar_today_outlined' },
+    { path: '/profile', label: 'Profile', icon: 'account_circle_outlined' },
+  ];
+
+
+  constructor(
+    public router: Router,
+    public snackBar: MatSnackBar,
+    public route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  logOut(): void {
+    localStorage.clear();
+    this.router.navigate(['/welcome']);
+    this.snackBar.open('You have successfully logged out!', 'OK', {
+      duration: 2000,
+    });
+  }
+
+}
